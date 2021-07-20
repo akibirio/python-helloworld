96bf9bfeec7fc19e2efa17ea6e682f4f3c395e18
ayubkibirio <ayubkibirio@gmail.com>
2021-07-20 10:46:47 +0300

    First Commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask import json
+import logging
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    ## log line
+    app.logger.info('Main Request successful')  
+    return "Hello World!"
+
+@app.route("/status")
+def status():
+    response = app.response_class(
+        response=json.dumps({"result":"OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+    ## log line
+    app.logger.info('Status Request successful')
+    return response
+ 
+ 
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(
+        response=json.dumps({"status":"success", "code":0, "data":{"UserCount":140,"UserActive":23}}),
+        status=200,
+        mimetype='application/json'
+    )
+    ## log line
+    app.logger.info('Metrics Request successful')
+    return response
+ 
+
+if __name__ == "__main__":
+
+    ## streamlogs to app.log file
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+    
+    app.run(host='0.0.0.0')
